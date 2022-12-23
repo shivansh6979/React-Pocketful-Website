457d1d3ffe611b6e8f08641909c68e9186a4184f
shivansh6979 <shivansh11g@gmail.com>
2022-12-23 11:29:16 +0530

    Added Headfer component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,9 +4,11 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import HomePage from "./components/HomePage.js";
 import Footer from "./components/Footer";
+import Header from "./components/Header";
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
         <Route exact path="/" element={<Login />} />
         <Route exact path="/home" element={<HomePage />} />
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./header.css";
+const Header = () => {
+  return <div className="header">React-Pace-Stock-Assignment</div>;
+};
+
+export default Header;
